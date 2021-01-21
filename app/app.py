5d877e6f74e8cd076fdcca8b801b7fba85e780c5
biomlds <45866279+biomlds@users.noreply.github.com>
2021-01-20 18:32:34 -0700

    Update: flask-mmdetect mvp

--- app/app.py
+++ app/app.py
@@ -1,30 +1,15 @@
-import os
-# from os.path import join, dirname, realpath
-from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory, send_file
-from flask_uploads import IMAGES, UploadSet, configure_uploads
-    
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, send_from_directory, send_file
+from flask_uploads import IMAGES, UploadSet, configure_uploads    
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+# from wtforms import FileField, SubmitField
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+# from wtforms.validators import FileAllowed, FileRequired
 from flask_uploads import configure_uploads, IMAGES, UploadSet
 
-
-
-# import some common libraries
 import numpy as np
-import os
-# from google.colab.patches import cv2_imshow
-# from PIL import Image
-
-
-
-# import some common detectron2 utilities
 from utils.model import run_model, get_model
-
-##################
-### Detectron2 ###
-##################
-
 
 ################
 ### Fask APP ###
@@ -39,7 +24,9 @@ images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
 
 class Upload(FlaskForm):
-    image = FileField('image')
+    image = FileField('image', validators=[
+        FileRequired(),
+        FileAllowed(images, 'Images only!')])
     submit = SubmitField('Submit')
 
 class Download(FlaskForm):
@@ -59,11 +46,7 @@ def index():
         print('processed_img: ', processed_img, '##################')
         model, result = run_model(img=img)
         model.show_result(img, result, score_thr=0.5, show=False,
-                    #wait_time=0.1,
-                    # fig_size=(5,5),
                     win_name=processed_filename,
-                    # bbox_color=(72, 101, 241),
-                    # text_color=(72, 101, 241),
                     out_file=processed_img) 
 
         return redirect(url_for('show', picture=filename))
@@ -80,16 +63,11 @@ def show(picture):
     processed_img = f"{app.config['UPLOADED_IMAGES_DEST']}{processed_filename}"
 
     if download_form.validate_on_submit():
-        print('send_file############processed_img:', processed_img)
-        print(app.root_path, app.config['UPLOADED_IMAGES_DEST'])
-        # return send_file('/flask_detect/app/static/img/'+processed_filename, as_attachment=True)
         return send_file(app.config['UPLOADED_IMAGES_DEST']+processed_filename, as_attachment=True)
-        # return send_from_directory(app.config['UPLOADED_IMAGES_DEST'],
-        #                        processed_filename, as_attachment=True)
     return render_template('show.html', pic=filename, form=download_form)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,debug=False,use_reloader=True)
+    app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
 
 
--- app/utils/model.py
+++ app/utils/model.py
@@ -2,10 +2,7 @@ import requests
 from os import chdir, system, path
 
 def get_model(weights, config):
-    #system('git clone https://github.com/open-mmlab/mmdetection.git')
-    #system('cd mmdetection')
-    #system('pip install -e .')
-    #system('cd ..')
+
     system('mkdir -p model_config')
 
     weights_path = f"model_config/{weights.split('/')[-1]}"
@@ -19,13 +16,9 @@ def get_model(weights, config):
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot, show_result_pyplot
 
 def run_model(img,
-              #weights='https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth',
               weights='model_configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth',
               config='../mmdetection/configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_1x_coco.py'):
-    
-    #get_model(weights, config)
-    #weights_path = f"model_config/{weights.split('/')[-1]}"
-    #print(config_path, weights_path)
+
     model = init_detector(config, weights, device='cpu')
     # Use the detector to do inference
     result = inference_detector(model, img)
